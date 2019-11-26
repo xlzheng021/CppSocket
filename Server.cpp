d8d900d32af7738be03cc4d4610e1fb18bb3fa44
Xiaolong Zheng <xzheng@ah-xzheng-maci.dhcp.mathworks.com>
2019-11-26 16:39:22 -0500

    async server

--- Server.cpp
+++ Server.cpp
@@ -7,30 +7,36 @@ using std::string;
 using std::cout;
 using std::endl;
 
+
 string read_(tcp::socket & socket) {
-       boost::asio::streambuf buf;
-       boost::asio::read_until( socket, buf, "\n" );
-       string data = boost::asio::buffer_cast<const char*>(buf.data());
-       return data;
+  boost::asio::streambuf buf;
+  boost::asio::read_until( socket, buf, "\n" );
+
+string data = boost::asio::buffer_cast<const char*>(buf.data());
+  return data;
 }
+
+
 void send_(tcp::socket & socket, const string& message) {
-       const string msg = message + "\n";
-       boost::asio::write( socket, boost::asio::buffer(message) );
+  const string msg = message + "\n";
+  boost::asio::write( socket, boost::asio::buffer(message) );
 }
 
 int main() {
-      boost::asio::io_service io_service;
-//listen for new connection
-      tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234 ));
-//socket creation 
-      tcp::socket socket_(io_service);
-//waiting for connection
-      acceptor_.accept(socket_);
-//read operation
-      string message = read_(socket_);
-      cout << message << endl;
-//write operation
-      send_(socket_, "Hello From Server!");
-      cout << "Servent sent Hello message to Client!" << endl;
-   return 0;
+  boost::asio::io_service io_service;
+
+  tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234 ));
+  
+  tcp::socket socket_(io_service);
+  acceptor_.accept(socket_);
+
+ 
+    string message = read_(socket_);
+    cout << message << endl;
+
+    send_(socket_, "Hello From Server!");
+    cout << "Servent sent Hello message to Client!" << endl;
+
+  return 0;
 }
+
